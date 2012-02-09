d7a1307de5e605d0d3317cd539cff05440ec31bf
kuloiwa <kuloiwa@gmail.com>
2012-02-09 11:33:51 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,196 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  
+  <meta name="author" content="琉球舞団 昇龍祭太鼓" />
+  
+  <meta name="keywords" content="琉球舞団 昇龍祭太鼓 琉球 沖縄 エイサー 創作エイサー 太鼓 パーランク 獅子 龍 全国エイサー大会　グランプリ　受賞　世界エイサー大会　新宿エイサー　琉球國祭り太鼓" />
+  
+  <meta name="description" content="創作エイサー団体「琉球舞団　昇龍祭太鼓」のホームページです。東京を中心に日本各地や世界まで幅広い活動を行っております。演舞具の紹介、イベント日程の案内、団員募集、コミュニケーションペーパーバックナンバーのダウンロードなどができます。" />
+  
+  <title>琉球舞団 昇龍祭太鼓</title>
+  
+  <link href="css/stylesheet.css" rel="stylesheet" type="text/css" media="screen, projection, print" />
+  <link rel="shortcut icon" href="kokoromark.ico">
+  
+  <script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
+  
+  <script src="js/swfobject.js" type="text/javascript"></script>
+  
+  <!--[if IE]>
+  <script src="js/iepngfix.js" language="JavaScript" type="text/javascript"></script>
+  <![endif]-->
+  
+</head>
+<body>
+  <div id="back-shadow"><div id="back-obi"><div id="content">
+  
+  <!-- メインフラッシュ -->
+  <div id="top-flash">
+  
+    <div>
+    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="840" height="480" id="op-fla-c">
+      <param name="movie" value="op-fla-c.swf" />
+      <param name="FlashVars" value="home=0" />
+      <!--[if !IE]>-->
+        <object type="application/x-shockwave-flash" data="op-fla-c.swf" width="840" height="480">
+        <param name="FlashVars" value="home=0" />
+      <!--<![endif]-->
+      <div id="no-flash">
+        <img src="nonflash/top_nonflash.jpg" width="840" height="480" border="0" usemap="#topmainnav" />
+        <map name="topmainnav" id="topmainnav">
+        <area shape="rect" coords="556,9,642,51" href="etc/copyright.php" alt="著作権について" />
+        <area shape="rect" coords="647,8,731,50" href="etc/index.php" alt="サイトポリシー" />
+        <area shape="rect" coords="736,8,810,50" href="etc/sitemap.php" alt="サイトマップ" />
+        <area shape="rect" coords="33,56,159,88" href="about/index.php" alt="団体について" />
+        <area shape="rect" coords="162,56,287,88" href="member/index.php" alt="メンバー" />
+        <area shape="rect" coords="292,56,417,88" href="event/index.php/" alt="イベント情報" />
+        <area shape="rect" coords="423,56,548,88" href="photo/index.php" alt="写真＆映像" />
+        <area shape="rect" coords="552,56,678,88" href="contact/index.php" alt="お問い合わせ" />
+        </map>
+        <div id="aboutgetflash">
+          <p>このサイトはFLASHのバージョン9以上対応です。ご覧いただくためには、最新のFLASHをダウンロードしてください。 <a href="http://www.adobe.com/go/getflashplayer">最新版のFLASHをダウンロードする。</a></p>
+        </div>
+      </div>
+      
+      <!--[if !IE]>-->
+        </object>
+      <!--<![endif]-->
+    </object>
+  </div>  
+  </div>
+  
+  <div id="info">
+
+
+<!-- 120103 東日本大震災募金報告 -->
+
+
+
+    <!-- 最新情報 -->
+    <div id="joho">
+
+
+
+      <h3><img src="images/top/sub-saisin.gif" width="245" height="25" /></h3>
+      <div id="joho-content">
+        <div id="saishin">
+          <div class="box"><p class="date">03/22</p><p class="text">東北地方太平洋沖地震に対する義援金の募集を開始しました。</p></div>
+<div class="box"><p class="date">03/22</p><p class="text">2011年夏のイベントの一般参加者の募集を開始しました。</p></div>
+<div class="box"><p class="date">11/14</p><p class="text">10月17日（日）に行われた「2010全国エイサー大会　創作エイサーコンテスト」にてグランプリを受賞しました。</p></div>
+<div class="box"><p class="date">07/05</p><p class="text">「団体について」の英語版を公開しました。</p></div>
+        </div>
+        <!-- 更新情報 -->
+        <div id="koushin">
+          <h4>更新情報</h4>
+           <p class="text">出演予定を更新しました。（2012 02.08）</p>
+<p class="text">練習場所に「MO-LE STUDIO」を追加しました。（2012 02.03）</p>
+<p class="text">練習予定を更新しました。（2012 01.12）</p>
+<p class="text">トップページの写真と音楽が新しくなりました。トピックスを更新しました。会報「もーれ もーれ Vol.2」を追加しました。（2012 01.07）</p>
+        </div>
+
+
+        <a href="molemole/molemole_02.pdf" style="display:block; margin:18px 0 0 23px; width:230px; height:130px;" >
+            <img src="top/images/btn-gienkin-1201.gif" width="230" height="140" style="margin:0; padding:0;" />
+        </a>
+
+      </div>
+    </div>
+    
+    
+    
+    
+    <!-- 内容ここから -->
+    <div id="topics">
+      <h3><img src="images/top/sub-topics.gif" width="245" height="25" /></h3>
+      
+      <div id="topics-content">
+        
+        <h4><a href="topics/index.php" style="color:#FF0000; font-weight:bold;">世界エイサー大会2011</a></h4>
+        <div class="photo"><a href="topics/index.php" style="border:0;"><img src="topics/1201_sekaieisa/image/top.jpg" width="230" height="175" /></a></div>
+        <p class="text"><span style="color:#FFF; font-weight:bold;">&#187; </span><a href="topics/index.php" style="color:#FFFFFF; font-weight:bold;">詳しくはコチラ</a></p>
+        
+
+
+
+        <h4 style="border-top:1px dotted #666; margin-top:3px !important; padding-top:6px !important;"><a href="topics/1011_eisaokinawa.php" style="color:#FF0000; font-weight:bold;">2010全国エイサー大会で<br />栄光ある第1回グランプリ賞を受賞！</a></h4>
+        <div class="photo"><a href="topics/1011_eisaokinawa.php" style="border:0;"><img src="topics/1011_eisaokinawa/image/top.jpg" width="230" height="175" /></a></div>
+        
+        <p class="text"><span style="color:#FFF; font-weight:bold;">&#187; </span><a href="topics/1011_eisaokinawa.php" style="color:#FFFFFF; font-weight:bold;">詳しくはコチラ</a></p>
+
+        <!--
+        <h4 style="border-top:1px dotted #666; margin-top:3px !important; padding-top:6px !important;"><a href="topics/index.php" style="color:#FF0000; font-weight:bold;">世界エイサー大会2011</a></h4>
+        <div class="photo"><img src="topics/1201_sekaieisa/image/top.jpg" width="230" height="175" /></div>
+        <p class="text"><span style="color:#FFF; font-weight:bold;">&#187; </span><a href="topics/index.php" style="color:#FFFFFF; font-weight:bold;">詳しくはコチラ</a></p>
+        -->
+        
+      </div>
+    </div>
+    <!-- 内容ここまで -->
+    
+  
+    
+    <div id="button">
+      <ul><li><img src="images/top/btn-keitai.gif" width="200" height="102" id="keitai" /></li>
+        <li><a href="about/07-molemole.php"><img src="images/top/btn-molemole.gif" width="201" height="61" id="molemole" /></a></li>
+        <li><a href="event/03-kacyasi.php"><img src="images/top/btn-kachasi.gif" width="206" height="61" id="kachasi" /></a></li>
+        <li><a href="member/nyuubu.php"><img src="images/top/btn-sibuin-boshu.gif" width="200" height="38" id="sibuinboshu" /></a></li>
+
+
+        <li><!-- スタジオPR --><a href="http://www.mo-le.com" target="_blank"><img src="images/bn-mole.gif" width="198" height="40" id="" /></a></li>
+
+        <li><!-- スタジオPR --><a href="http://www.mo-le.com" target="_blank"><img src="images/bn-studio.png" width="202" height="90" id="" /></a></li>
+
+
+        <li><!-- 沖縄PR --><a href="http://www.ocvb.or.jp/index.php" target="_blank"><img src="images/pr-okinawa.jpg" width="201" height="126" id="okinawa" /></a></li>
+
+        <li><!-- ちゅら海水族館PR --><a href="http://oki-churaumi.jp" target="_blank"><img src="images/pr-churaumi.gif" width="201" height="61" id="churaumi" /></a></li>
+
+
+      </ul>
+    </div>
+
+<img src="images/top/info-back-bottom.gif" width="815" height="15" />
+
+
+
+
+    <br class="clear">
+
+  </div>
+  
+  <br class="clear">
+  
+  <!-- テキストナビ -->
+  <ul id="footer-nav">
+    <li><a href="about/index.php">団体について</a></li>
+    <li><a href="member/index.php">メンバー</a></li>
+    <li><a href="event/index.php">イベント情報</a></li>
+    <li><a href="photo/index.php">写真と映像</a></li>
+    <li><a href="contact/index.php">お問い合わせ</a></li>
+    <li class="sub"><a href="option/copyright.php">著作権について</a></li>
+    <li class="sub"><a href="option/index.php">サイトポリシー</a></li>
+    <li class="sub"><a href="option/sitemap.php">サイトマップ</a></li>
+  </ul>
+  
+  <br class="clear">
+  
+  <img src="images/bottom-bar.gif" alt="琉球舞団 昇龍祭太鼓" width="840" height="8" id="footer-bar" />
+  <div id="footer">Copyright &#169; RYUKYU PROJECT</div>
+  
+</div></div></div>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8170471-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</body>
+</html>
+
